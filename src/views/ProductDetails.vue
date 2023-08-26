02e7a24769233bc82732d61a5d1a4a03d5bfd4c7
ibrahemshabanah2022 <113831449+ibrahemshabanah2022@users.noreply.github.com>
2023-08-26 19:12:24 +0300

    add comment review to product

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -50,6 +50,7 @@
               <i class="fa fa-shopping-cart mr-1"></i> Add To Cart
             </button>
           </div>
+
           <div class="d-flex pt-2">
             <strong class="text-dark mr-2">Share on:</strong>
             <div class="d-inline-flex">
@@ -69,6 +70,104 @@
           </div>
         </div>
       </div>
+      <div class="row px-xl-5">
+        <div class="col">
+          <div class="bg-light p-30">
+            <div class="nav nav-tabs mb-4">
+              <a
+                class="nav-item nav-link text-dark active"
+                data-toggle="tab"
+                href="#tab-pane-1"
+                >Description</a
+              >
+
+              <a
+                class="nav-item nav-link text-dark"
+                data-toggle="tab"
+                href="#tab-pane-3"
+                >Reviews
+              </a>
+            </div>
+            <div class="tab-content">
+              <div class="tab-pane fade show active" id="tab-pane-1">
+                <h4 class="mb-3">Product Description</h4>
+                <p>
+                  {{ product.description }}
+                </p>
+              </div>
+
+              <div class="tab-pane fade" id="tab-pane-3">
+                <div class="row">
+                  <div class="col-md-6">
+                    <!-- <h4 class="mb-4">1 review for "Product Name"</h4> -->
+                    <div class="media mb-4">
+                      <div class="media-body">
+                        <h6>
+                          John Doe<small> - <i>01 Jan 2045</i></small>
+                        </h6>
+                        <!-- <div class="text-primary mb-2">
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star"></i>
+                          <i class="fas fa-star-half-alt"></i>
+                          <i class="far fa-star"></i>
+                        </div> -->
+                        <p>
+                          Diam amet duo labore stet elitr ea clita ipsum, tempor
+                          labore accusam ipsum et no at. Kasd diam tempor rebum
+                          magna dolores sed sed eirmod ipsum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <h4 class="mb-4">Leave a review</h4>
+
+                    <!-- <div class="d-flex my-3">
+                      <p class="mb-0 mr-2">Your Rating * :</p>
+                      <div class="text-primary">
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                      </div>
+                    </div> -->
+                    <form>
+                      <div class="form-group">
+                        <label for="message">Your Review *</label>
+                        <form @submit.prevent="createComment">
+                          <div>
+                            <textarea
+                              id="message"
+                              cols="30"
+                              rows="5"
+                              class="form-control"
+                              v-model="commentContent"
+                            ></textarea>
+                          </div>
+                          <br />
+                          <div>
+                            <button class="btn btn-primary px-3" type="submit">
+                              Leave Your Review
+                            </button>
+                          </div>
+                        </form>
+                        <!-- <textarea
+                          id="message"
+                          cols="30"
+                          rows="5"
+                          class="form-control"
+                        ></textarea> -->
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <!-- Shop Detail End -->
@@ -76,6 +175,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
 export default {
@@ -86,6 +187,7 @@ export default {
   data() {
     return {
       product: {},
+      commentContent: "",
     };
   },
   mounted() {
@@ -97,6 +199,36 @@ export default {
       });
   },
   methods: {
+    createComment() {
+      const id = this.$route.params.id;
+
+      const userToken = localStorage.getItem("userToken");
+
+      fetch("http://localhost:8000/api/PostComment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userToken}`,
+        },
+        body: JSON.stringify({
+          content: this.commentContent,
+          product_id: id,
+        }),
+      })
+        // axios
+        //   .post("http://localhost:8000/api/PostComment", {
+        //     content: this.commentContent,
+        //     product_id: id,
+        //   })
+        .then((response) => {
+          console.log(response.data);
+          // handle success response
+        })
+        .catch((error) => {
+          console.log(error);
+          // handle error response
+        });
+    },
     showSwal() {
       let productt = JSON.parse(localStorage.getItem("product")) || [];
       const tIndex = productt.findIndex((p) => p.id === this.product.id);
